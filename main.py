37d6256909aef3ab788f99f376beaf7e89b6b583
eugene-cheung <eugenus1@uw.edu>
2025-02-14 13:06:43 -0800

    starter code for pyqt6 app

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import sys
+import os
+from time import strftime, gmtime
+import threading
+from time import sleep
+from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtQuick import QQuickWindow
+from PyQt6.QtCore import QObject, pyqtSignal
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Backend(QObject):
 
+    def __init__(self):
+        QObject.__init__(self)
+    updated = pyqtSignal(str, arguments=['updater'])
+    def updater(self, curr_time):
+        self.updated.emit(curr_time)
+    def bootUp(self):
+        t_thread = threading.Thread(target=self._bootUp)
+        t_thread.daemon = True
+        t_thread.start()
+    def _bootUp(self):
+        while True:
+            curr_time = strftime("%H:%M:%S", gmtime())
+            self.updater(curr_time)
+            sleep(0.1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+QQuickWindow.setSceneGraphBackend('software')
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load('./UI/main.qml')
+back_end = Backend()
+engine.rootObjects()[0].setProperty('backend', back_end)
+back_end.bootUp()
+sys.exit(app.exec())
